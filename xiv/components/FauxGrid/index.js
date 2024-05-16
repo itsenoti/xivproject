5bdf974b5f899c604b366843990a3a30d5ee59e5
itsenoti <fretz.villanueva@gmail.com>
2024-05-16 18:40:37 +0800

    [Faux Hallows] Added Set 1-1

--- xiv/components/FauxGrid/index.js
+++ xiv/components/FauxGrid/index.js
@@ -1,13 +1,40 @@
 import style from "./FauxGrid.module.css";
 
-function FauxGrid(props) {
+const DISABLED = "1";
+const SWORD = "2";
+const TREASURE_BOX = "3";
+const FOX = "4";
+const RECOMMENDED = "5";
+
+function FauxGrid({ disabledCells }) {
+  // 1: Disabled    2: Sword     3: Treasure Box      4: Fox    5: Recommended
+
   var allCells = [];
+  var cellContent = "0";
+
   for (let i = 0; i < 36; i++) {
-    allCells.push(
-      <div
-        className={props.disabledCells[i] == "1" ? style.fauxGridDisabledCell : style.fauxGridCell}
-      ></div>
-    );
+    switch (disabledCells[i]) {
+      case DISABLED:
+        cellContent = style.fauxGridDisabledCell;
+        break;
+      case SWORD:
+        cellContent = style.fauxGridSword;
+        break;
+      case TREASURE_BOX:
+        cellContent = style.fauxGridTreasureBox;
+        break;
+      case FOX:
+        cellContent = style.fauxGridFox;
+        break;
+      case RECOMMENDED:
+        cellContent = style.fauxGridRecommended;
+        break;
+      default:
+        cellContent = style.fauxGridCell;
+        break;
+    }
+
+    allCells.push(<div className={cellContent}></div>);
   }
   return <div className={style.fauxGridRowContainer}>{allCells}</div>;
 }
--- xiv/pages/FauxHallow/set_1.js
+++ xiv/pages/FauxHallow/set_1.js
@@ -2,34 +2,68 @@ import FauxGrid from "../../components/FauxGrid";
 import style from "./FauxHallow.module.css";
 
 export default function Set_1() {
-  const disabledCells = "000000001010010000000000001000000001";
+  // 1: Disabled    2: Sword     3: Treasure Box      4: Fox
+  const coloredCells = "000000 001010 010000 000050 001000 000001";
 
   return (
     <>
-      <div>
+      <div className={style.row}>
         <div className={style.fauxGridContainer}>
-          <FauxGrid disabledCells={disabledCells} />
+          <FauxGrid disabledCells={"330440331010410022000052001022400001"} />
         </div>
         <div className={style.fauxGridContainer}>
-          <FauxGrid disabledCells={disabledCells} />
-        </div>
-        <div className={style.fauxGridContainer}>
-          <FauxGrid disabledCells={disabledCells} />
-        </div>
-        <div className={style.fauxGridContainer}>
-          <FauxGrid disabledCells={disabledCells} />
+          <FauxGrid disabledCells={"000440001010413322003352001022400001"} />
         </div>
       </div>
 
-      <div>
+      <div className={style.row}>
         <div className={style.fauxGridContainer}>
-          <FauxGrid disabledCells={disabledCells} />
+          <FauxGrid disabledCells={"330000331410010224000250001220000441"} />
         </div>
         <div className={style.fauxGridContainer}>
-          <FauxGrid disabledCells={disabledCells} />
+          <FauxGrid disabledCells={"000000001410010224330250331220000441"} />
+        </div>
+      </div>
+
+      <div className={style.row}>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"330004331010010440004250001220000221"} />
         </div>
         <div className={style.fauxGridContainer}>
-          <FauxGrid disabledCells={disabledCells} />
+          <FauxGrid disabledCells={"000004001010010440334250331220000221"} />
+        </div>
+      </div>
+
+      <div className={style.row}>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"330004331010010440004252001222000001"} />
+        </div>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"000004001010010440004252331222330001"} />
+        </div>
+      </div>
+
+      <div className={style.row}>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"334000331014012220002250001004004001"} />
+        </div>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"004000001014012220002250331004334001"} />
+        </div>
+      </div>
+
+      <div className={style.row}>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"004000001014010000220050221334224331"} />
+        </div>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"004000001014010033220053221004224001"} />
+        </div>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"000000001410010004220053221033220441"} />
+        </div>
+        <div className={style.fauxGridContainer}>
+          <FauxGrid disabledCells={"000000001410013304223350221000220441"} />
         </div>
       </div>
     </>
