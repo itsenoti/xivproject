6ffc25cd247ea0a64dd6f3f6a088d3027bde9011
itsenoti <fretz.villanueva@gmail.com>
2024-06-24 11:01:32 +0800

    Update _document.js

--- xiv/pages/_document.js
+++ xiv/pages/_document.js
@@ -1,13 +1,11 @@
-"use client";
-
 import { Head, Html, Main, NextScript } from "next/document";
-import { poppins } from "../public/fonts/fonts";
+import { poppins } from "../public/fonts/fonts.js";
 
 export default function Document() {
   return (
     <Html lang="en" className={poppins.className}>
       <Head />
-      <body>
+      <body sx={{ fontFamily: "Poppins" }}>
         <Main />
         <NextScript />
       </body>
